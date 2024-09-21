481e0c49db4a10e6f62f829a6f9f084960b645c3
Mohammed Sultan Rashid Al-Abri <54262813+Mohammed-Alabri@users.noreply.github.com>
2024-09-21 06:45:19 +0400

    Delete flagy/MD7H/decode.py

--- flagy/MD7H/decode.py
+++
@@ -1,8 +0,0 @@
-hashh = b"IQ\x01CM\x00fS2\x0f\"\x0fX$\x11EXiiX$XEi\x01$\"X\x0c\x0fff\x012fSf2h"
-res = ""
-for i in hashh:
-    for j in range(1, 256):
-        if i == (j * 0x34) % 0x7b:
-            res += chr(j)
-            break
-print(res)
